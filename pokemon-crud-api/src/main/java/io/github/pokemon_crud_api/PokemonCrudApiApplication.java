70e2af0adc0386ca395859776af7b9c7d4fcb3ab
Manytesting369 <gerallujan0721@gmail.com>
2025-05-31 12:22:34 -0500

    Creación backend crud

---
+++ pokemon-crud-api/src/main/java/io/github/pokemon_crud_api/PokemonCrudApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.pokemon_crud_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonCrudApiApplication.class, args);
+	}
+
+}
---
+++ pokemon-crud-api/src/test/java/io/github/pokemon_crud_api/PokemonCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pokemon_crud_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
